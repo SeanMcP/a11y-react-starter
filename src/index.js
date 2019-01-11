389aa41817bef8e1ac05d3be850fd43de7f881bf
Sean McPherson <seanmcp25@gmail.com>
2019-01-11 16:52:00 -0500

    Add react-a11y, jsx-a11y

--- src/index.js
+++ src/index.js
@@ -3,6 +3,14 @@ import ReactDOM from 'react-dom';
 import './styles/index.css';
 import App from './components/App';
 import * as serviceWorker from './serviceWorker';
+import { standard as rules } from './a11y/react-a11y-rules';
+
+if (process.env.NODE_ENV === 'development') {
+    const a11y = require('react-a11y').default;
+    a11y(React, ReactDOM, {
+        rules
+    });
+}
 
 ReactDOM.render(<App />, document.getElementById('root'));
 

0d4e1f753a78fb7646f2765476291267b2bdc3e5
Sean McPherson <seanmcp25@gmail.com>
2019-01-14 20:06:32 -0500

    Create standard ruleset

--- src/a11y/reactA11yRules.js
+++ src/a11y/reactA11yRules.js
@@ -2,32 +2,36 @@
     By default, all react-a11y rules are included. Comment
     out any rules that don't fit your particular use case.
 
-    Link to documentation: https://github.com/reactjs/react-a11y/tree/master/docs/rules
+    Link to documentation:
+    https://github.com/reactjs/react-a11y/tree/master/docs/rules
 */
 
-const rules = [
-    'aria-role',
-    'aria-unsupported-elements',
-    'click-events-have-key-events',
-    'hidden-uses-tabindex',
-    'img-redundant-alt',
-    'img-uses-alt',
-    'interactive-supports-focus',
-    'label-has-for',
-    'mouse-events-have-key-events',
-    'no-access-key',
-    'no-hash-ref',
-    'no-onchange',
-    'onclick-uses-role',
-    'tabindex-no-positive',
-    'tabindex-uses-button'
-];
+export const standard = {
+    'aria-role': 'error',
+    'aria-unsupported-elements': 'warn',
+    'click-events-have-key-events': 'error',
+    'hidden-uses-tabindex': 'warn',
+    'img-redundant-alt': 'warn',
+    'img-uses-alt': 'error',
+    'interactive-supports-focus': 'warn',
+    'label-has-for': 'warn',
+    'mouse-events-have-key-events': 'error',
+    'no-access-key': 'warn',
+    'no-hash-ref': 'warn',
+    'no-onchange': 'warn',
+    'onclick-uses-role': 'error',
+    'tabindex-no-positive': 'warn',
+    'tabindex-uses-button': 'warn'
+};
 
+// For bulk rule generation
 function generateRules(setting) {
-    const output = {};
-    rules.forEach((rule) => (output[rule] = setting));
+    const output = { ...standard };
+    for (const key in output) {
+        output[key] = setting;
+    }
     return output;
 }
 
-export const standard = generateRules('warn');
+export const light = generateRules('warn');
 export const strict = generateRules('error');
